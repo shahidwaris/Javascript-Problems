7859abfbcc3c0d63f7b3a4baba13724b7c667d4b
shahidwaris <71723136+shahidwaris@users.noreply.github.com>
2023-04-27 04:40:58 +0530

    Create 43_1_prob.cy.js

---
+++ 43_1_prob.cy.js
@@ -0,0 +1,43 @@
+describe('Shape and Circle classes', () => {
+  it('creates an instance of Shape with the correct properties', () => {
+    const shape = new Shape(0, 0);
+    expect(shape.x).to.equal(0);
+    expect(shape.y).to.equal(0);
+  });
+
+  it('creates an instance of Circle with the correct properties', () => {
+    const circle = new Circle(1, 1, 5);
+    expect(circle.x).to.equal(1);
+    expect(circle.y).to.equal(1);
+    expect(circle.radius).to.equal(5);
+  });
+
+  it('moves Shape to the correct position', () => {
+    const shape = new Shape(0, 0);
+    shape.moveTo(1, 2);
+    expect(shape.x).to.equal(1);
+    expect(shape.y).to.equal(2);
+  });
+
+  it('moves Circle to the correct position', () => {
+    const circle = new Circle(1, 1, 5);
+    circle.moveTo(2, 3);
+    expect(circle.x).to.equal(4);
+    expect(circle.y).to.equal(6);
+  });
+
+  it('does not modify Shape after Circle moves', () => {
+    const shape = new Shape(0, 0);
+    const circle = new Circle(1, 1, 5);
+    circle.moveTo(2, 3);
+    expect(shape.x).to.equal(0);
+    expect(shape.y).to.equal(0);
+  });
+
+  it('moves Circle with correct x and y coordinates', () => {
+    const circle = new Circle(1, 1, 5);
+    circle.moveTo(2, 3);
+    expect(circle.x).to.equal(4);
+    expect(circle.y).to.equal(6);
+  });
+});
